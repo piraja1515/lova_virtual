fa70c0e67115b828e55dc02f0f7d256e4112ea01
siqueira <siqueira1515@hotmail.com>
2023-03-29 20:18:34 -0300

    commit inicial projeto loja virtual java

---
+++ src/main/java/estudolova_virtual/LovaVirtualApplication.java
@@ -0,0 +1,13 @@
+package estudolova_virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovaVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/estudolova_virtual/LovaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package estudolova_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
